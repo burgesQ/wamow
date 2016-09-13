540233aa8210c6f709ad087115549b9cf0e7eefe
Lead Dev <leaddev@iMac-de-Administrator.local>
2016-09-13 18:42:34 +0200

    | ADD => ProfileController.php : overload showAction | MOD => profile.xml : modify routing for overload controller | MOD => show_content.html.twig : customise show view

---
+++ symfony/src/UserBundle/Controller/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\Model\UserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Controller\ProfileController as BaseController;
+
+class ProfileController extends BaseController
+{
+    /**
+     * Show the user
+     */
+    public function showAction()
+    {
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->render('UserBundle:Profile:show.html.twig', array(
+            'user' => $user
+        ));
+    }
+}
