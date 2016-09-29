583300a88257a84434bf26ec532204b2cb3c7324
Myriam CHOLET <myriam@wantmore.work>
2016-09-29 15:03:14 +0200

    | MOD => src/ToolsBundle/Entity/Address.php : two new field : label - name | MOD => src/ToolsBundle/Form/AddressType.php : add label and name in the form | MOD => src/MissionBundle/Resources/views/Mission/new.html.twig : add label and name in the form view | MOD => src/MissionBundle/Resources/views/Mission/view.html.twig : add label and name in the final view

--- symfony/src/ToolsBundle/Form/AddressType.php
+++ symfony/src/ToolsBundle/Form/AddressType.php
@@ -5,6 +5,7 @@ namespace ToolsBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class AddressType extends AbstractType
 {
@@ -23,7 +24,10 @@ class AddressType extends AbstractType
             ->add('country', 'country', array(
                 'required' => true,
                 'placeholder' => 'Choose a country')
-            );
+            )
+            ->add('label')
+            ->add('name')
+            ;
     }
 
     /**
