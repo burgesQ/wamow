0b50920114dff515e0d7030a7fa903ad06757d0f
Myriam CHOLET <myriam@wantmore.work>
2016-09-30 10:51:03 +0200

    | MOD => src/ToolsBundle/Controller/ToolsController.php : test lines removed | DEL => src/ToolsBundle/Resources/views/Tools/index.html.twig : hello world removed

--- symfony/src/ToolsBundle/Controller/ToolsController.php
+++ symfony/src/ToolsBundle/Controller/ToolsController.php
@@ -7,18 +7,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use MissionBundle\Entity\Step;
-use MissionBundle\Entity\Mission;
-use MissionBundle\Form\MissionType;
-use ToolsBundle\Entity\Language;
 
 class ToolsController extends Controller
 {
     public function indexAction()
     {
-        $language = new Language();
-        $language->setName("french");
-        return $this->render('ToolsBundle:Tools:index.html.twig');
     }
 
 }
