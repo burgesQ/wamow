e75303e3f9a10d169e60e83290d9a964ad83df4f
Myriam CHOLET <myriam@wantmore.work>
2016-10-10 12:56:16 +0200

    | MOD => src/MissionBundle/Controller/MissionController.php : add step config from json + deleted old step function | MOD => src/MissionBundle/Resources/config/services.yml : services paths | ADD => src/ToolsBundle/Config/mission.json : step config | ADD => src/ToolsBundle/Entity/Config.php : key / value | ADD => src/ToolsBundle/Service/MissionNbStep.php : add services to get value from the json

--- symfony/src/MissionBundle/Controller/MissionController.php
+++ symfony/src/MissionBundle/Controller/MissionController.php
@@ -10,8 +10,6 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use MissionBundle\Entity\Step;
 use MissionBundle\Entity\Mission;
 use MissionBundle\Form\MissionType;
-use MissionBundle\Form\StepType;
-use ToolsBundle\Entity\Language;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
@@ -25,13 +23,16 @@ class MissionController extends Controller
       $mission = new Mission();
       $form = $this->get('form.factory')->create(new MissionType(), $mission);
       $form->handleRequest($request);
+      $em = $this->getDoctrine()->getManager();
+      $service = $this->container->get('mission.nbStep');
+      $nbStep = $service->getMissionNbStep();
       if ($form->isValid())
         {
-          $em = $this->getDoctrine()->getManager();
           $em->persist($mission);
-          for ($i=0; $i < $mission->getNumberStep(); $i++)
+          for ($i=0; $i < $nbStep; $i++)
               {
-                  $step = new Step();
+                  $array = $service->getMissionStep($i);
+                  $step = new Step($array["nbMaxTeam"], $array["reallocTeam"]);
                   $step->setMission($mission);
                   $step->setPosition($i + 1);
                   $em->persist($step);
@@ -93,80 +94,5 @@ class MissionController extends Controller
         'mission'           => $mission,
         'listLanguage'      => $listLanguage,
         ));
-    }
-
-    /*
-      Add step to a mission
-    */
-    public function stepAction($id, Request $request)
-    {
-      $em = $this->getDoctrine()->getManager();
-      $mission = $em->getRepository('MissionBundle:Mission')->find($id);
-      $step = new Step();
-      $step->setMission($mission);
-      if (null === $mission)
-        {
-          throw new NotFoundHttpException("The mission ".$id." doesn't exist.");
-        }
-      $form = $this->get('form.factory')->create(new StepType(), $step);
-      $listStep = $em
-      ->getRepository('MissionBundle:Step')
-      ->findBy(array('mission' => $mission))
-      ;
-      $nbStep = 0;
-      foreach ($listStep as $step)
-        {
-          ++$nbStep;
-        }
-      if (!$listStep)    //If there's no step yet
-        {
-          if ($form->handleRequest($request)->isValid())
-            {
-              $em = $this->getDoctrine()->getManager();
-              $em->persist($step);
-              $em->flush();
-              $id = $mission->getid();
-              return $this->render('MissionBundle:Mission:stepdone.html.twig', array(
-                'mission'  =>   $mission,
-                'id'       =>   $id,
-                'form'     =>   $form,
-                'step'     =>   $step
-                ));
-            }
-          return $this->render('MissionBundle:Mission:step.html.twig', array(
-            'mission'  => $mission,
-            'step'     => $step,
-            'form'     => $form->createView()
-            ));
-        }
-      else if ($mission->getNumberStep() == $nbStep)  //If it's the last step
-      {
-        return new Response("Last step");
-      }
-      else                            //Creation of new step
-      {
-        $step = new Step();
-        $step->setMission($mission);
-        $step->setPosition($nbStep + 1);
-        if ($form->handleRequest($request)->isValid())
-          {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($step);
-            $em->flush();
-            $id = $mission->getid();
-            return $this->render('MissionBundle:Mission:stepdone.html.twig', array(
-              'mission'  =>   $mission,
-              'id'       =>   $id,
-              'form'     =>   $form,
-              'step'     =>   $step
-            ));
-          }
-        return $this->render('MissionBundle:Mission:step.html.twig', array(
-          'mission'  => $mission,
-          'step'     => $step,
-          'form'     => $form->createView(),
-          'nbStep'   => $nbStep
-          ));
-      }
     }
 }
