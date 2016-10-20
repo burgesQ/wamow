32ac464cf8f7a5e386123ad2704c12d6cc2aa57d
Quentin BURGESS <quentin@wantmore.work>
2016-10-20 18:48:34 +0200

    | ADD => src/ToolsBundle/Entity/Upload.php : upload entity | ADD => src/ToolsBundle/Form/UploadType.php : upload form | ADD => src/ToolsBundle/Repository/UploadRepository.php : auto-gen | MOD => src/ToolsBundle/Form/AddressType.php : merge and clean w/ Tools | MOD => src/ToolsBundle/Form/PhoneNumberType.php : merge and clean w/ Tools | MOD => src/UserBundle/Resources/views/Profile/show_content.html.twi : merge and clean w/ Tools

---
+++ symfony/src/ToolsBundle/Entity/Upload.php
@@ -0,0 +1,302 @@
+<?php
+
+namespace ToolsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+use UserBundle\Entity\User;
+
+/**
+ * Upload
+ *
+ * @ORM\Table(name="upload")
+ * @ORM\HasLifecycleCallbacks
+ * @ORM\Entity(repositoryClass="ToolsBundle\Repository\UploadRepository")
+ */
+class Upload
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="uploadDate", type="datetime", nullable=false)
+     */
+    private $uploadDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255, nullable=true)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="format", type="string", length=255, nullable=true)
+     */
+    private $format;
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     */
+    private $file;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="path", type="string", length=255, nullable=true)
+     */
+    private $path;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="UserBundle\Entity\User", cascade={"persist"})
+     * @ORM\JoinColumn(nullable=true)
+     */
+    private $user;
+
+    public function __construct($type = null)
+    {
+        $this->type = $type;
+        $this->uploadDate = new \Datetime();
+        $this->user = NULL;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->file) {
+            return;
+        }
+
+        $info = explode("/", $this->getFile()->getMimeType());
+
+        if ($this->getType() !== $info[0]) {
+            dump("ERRooooooororrororororororororotrololo");
+            die();
+            return ;
+        }
+
+        $str = $this->id;
+        $str .= time();
+        $str .= '.'.$info[1];
+
+        $this->getFile()->move(
+            $this->getUploadRootDir(),
+            $str);
+
+        $this->setName($str);
+        $this->setFormat($info[1]);
+        $this->setPath($this->getUploadRootDir().$str);
+    }
+
+    public function getUploadDir()
+    {
+        return 'uploads/';
+    }
+
+    public function getUploadRootDir()
+    {
+        return __DIR__.'/../../../web/'.$this->getUploadDir();
+    }
+
+    public function getWebPath()
+    {
+        return $this->getUploadDir().$this->getName();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \UserBundle\Entity\User $user
+     * @return Upload
+     */
+    public function setUser(\UserBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \UserBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set uploadDate
+     *
+     * @param \DateTime $uploadDate
+     * @return Upload
+     */
+    public function setUploadDate($uploadDate)
+    {
+        $this->uploadDate = $uploadDate;
+
+        return $this;
+    }
+
+    /**
+     * Get uploadDate
+     *
+     * @return \DateTime
+     */
+    public function getUploadDate()
+    {
+        return $this->uploadDate;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Upload
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Upload
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set format
+     *
+     * @param string $format
+     * @return Upload
+     */
+    public function setFormat($format)
+    {
+        $this->format = $format;
+
+        return $this;
+    }
+
+    /**
+     * Get format
+     *
+     * @return string
+     */
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     * @return Upload
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+
+    /**
+     * Set file
+     *
+     * @param string $file
+     * @return Upload
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    /**
+     * Get file
+     *
+     * @return string
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+}
