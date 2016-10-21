462711f2f5a42a007edc9bc3e84345caa6db7f44
jeromeheitor <jerome.heitor@epitech.eu>
2016-10-21 18:53:47 +0200

    MOD => Modified Company entity + added data fixtures for Sector

--- symfony/src/CompanyBundle/Controller/CompanyController.php
+++ symfony/src/CompanyBundle/Controller/CompanyController.php
@@ -10,27 +10,34 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use CompanyBundle\Entity\Company;
 use CompanyBundle\Form\CompanyType;
 
+
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
 class CompanyController extends Controller
 {
   public function createAction(Request $request)
     {
-    $user = $this->getUser();
-    $company = new Company($user->getId());
-    $form = $this->get('form.factory')->create(new CompanyType(), $company);
-    $form->handleRequest($request);
-    if ($form->isValid())
-        {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($company);
-            $em->flush();
-            $id = $mission->getId();
-            return $this->render('CompanyBundle:Company:registered.html.twig', array(
-            'mission'  =>   $company,
-            'form'     =>   $form,
-            ));
-        }
-    return $this->render('CompanyBundle:Company:new.html.twig', array(
-        'form' => $form->createView(),
-        ));
+    // $user = $this->getUser();
+    // $company = new Company();
+    //
+    // $form = $this->get('form.factory')->create(new CompanyType(), $company);
+    // $form->handleRequest($request);
+    // if ($form->isValid())
+    //     {
+    //         $em = $this->getDoctrine()->getManager();
+    //         $em->persist($company);
+    //         $em->flush();
+    //         $id = $mission->getId();
+    //         return $this->render('CompanyBundle:Company:registered.html.twig', array(
+    //         'mission'  =>   $company,
+    //         'form'     =>   $form,
+    //         ));
+    //     }
+      return $this->render('CompanyBundle:Default:index.html.twig');//, array(
+    //     'form' => $form->createView(),
+    //     ));
       }
 }
