0117de0fff38106a4aad12965f17f226a83fd514
jeromeheitor <jerome.heitor@epitech.eu>
2016-10-22 21:52:04 +0200

    MOD => src/CompanyBundle/Controller/CompanyController.php : Added create page + join page for company

--- symfony/src/CompanyBundle/Controller/CompanyController.php
+++ symfony/src/CompanyBundle/Controller/CompanyController.php
@@ -10,34 +10,62 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use CompanyBundle\Entity\Company;
 use CompanyBundle\Form\CompanyType;
 
-
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class CompanyController extends Controller
 {
+  /**
+   * @Security("has_role('ROLE_CONTRACTOR')")
+   */
   public function createAction(Request $request)
     {
-    // $user = $this->getUser();
-    // $company = new Company();
-    //
-    // $form = $this->get('form.factory')->create(new CompanyType(), $company);
-    // $form->handleRequest($request);
-    // if ($form->isValid())
-    //     {
-    //         $em = $this->getDoctrine()->getManager();
-    //         $em->persist($company);
-    //         $em->flush();
-    //         $id = $mission->getId();
-    //         return $this->render('CompanyBundle:Company:registered.html.twig', array(
-    //         'mission'  =>   $company,
-    //         'form'     =>   $form,
-    //         ));
-    //     }
-      return $this->render('CompanyBundle:Default:index.html.twig');//, array(
-    //     'form' => $form->createView(),
-    //     ));
+    $company = new Company();
+
+    $form = $this->get('form.factory')->createBuilder('form', $company)
+      ->add('name',      'text')
+      ->add('size',      'text')
+      ->add('logo',      'text')
+      ->add('resume',    'textarea')
+      ->add('sector',  EntityType::class, array(
+            'class' => 'CompanyBundle:Sector',
+            'choice_label' => 'name',))
+      ->add('save',      'submit')
+      ->getForm()
+    ;
+    $form->handleRequest($request);
+
+      if ($form->isValid()) {
+      $contractor = $this->getUser();
+      $company->setContractors($contractor);
+      $company->setType('no');
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($company);
+      $em->flush();
+
+      $request->getSession()->getFlashBag()->add('notice', 'Company successfully registered');
+      return $this->render('CompanyBundle:Default:index.html.twig', array('id' => $company->getId()));
+    }
+
+  return $this->render('CompanyBundle:Default:create.html.twig', array(
+     'form' => $form->createView(),));
       }
+  /**
+   * @Security("has_role('ROLE_CONTRACTOR')")
+   */
+  public function joinAction(Request $request)
+    {
+      $form = $this->get('form.factory')->createBuilder('form')
+        ->add('company',  EntityType::class, array(
+              'class' => 'CompanyBundle:Company',
+              'choice_label' => 'name',))
+        ->getForm();
+        return $this->render('CompanyBundle:Default:join.html.twig', array(
+           'form' => $form->createView(),));
+    }
 }
