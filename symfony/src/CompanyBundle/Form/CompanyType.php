0779d6f210fbad7b546c6e8abd5175408179f3f1
Anastasia DERUELLE <anastasia@wantmore.work>
2016-11-24 15:29:11 +0100

    | MOD [RefactoringCompany] - deleting useless twig - configuration ux - modification for symfony 3 - adding and refactoring translations - reorganise forms

--- symfony/src/CompanyBundle/Form/CompanyType.php
+++ symfony/src/CompanyBundle/Form/CompanyType.php
@@ -2,10 +2,14 @@
 
 namespace CompanyBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use CompanyBundle\Entity\Sector;
 
 class CompanyType extends AbstractType
@@ -17,7 +21,7 @@ class CompanyType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name',  'text', array(
+            ->add('name', TextareaType::class, array(
                 'required' => true,
                 'label' => 'company.new.name',
                 'translation_domain' => 'CompanyBundle',
@@ -25,35 +29,36 @@ class CompanyType extends AbstractType
             ->add('size', ChoiceType::class, array(
                 'translation_domain' => 'CompanyBundle',
                 'choices' => array(
-                    '0' => 'company.new.small',
-                    '1' => 'company.new.medium',
-                    '2' => 'company.new.big',
+                    'company.new.small' => '0',
+                    'company.new.medium' => '1',
+                    'company.new.big' => '2',
                 ),
+                'choices_as_values' => true,
                 'required'    => true,
                 'label' => 'company.new.size',
                 'placeholder' => 'company.new.choosesize',
                 'empty_data'  => null
             ))
-            ->add('logo',  'text', array(
+            ->add('logo', TextType::class, array(
                 'required' => true,
                 'label' => 'company.new.logo',
                 'translation_domain' => 'CompanyBundle',
             ))
-            ->add('resume',  'textarea', array(
+            ->add('resume', TextareaType::class, array(
                 'required' => true,
                 'label' => 'company.new.description',
                 'translation_domain' => 'CompanyBundle',
             ))
-            ->add('sector',   'entity', array(
+            ->add('sector', EntityType::class, array(
                 'class' => 'CompanyBundle:Sector',
-                'property' => 'name',
+                'choice_label' => 'name',
                 'multiple' => false,
                 'required' => true,
                 'placeholder' => 'company.new.choosesector',
                 'label' => 'company.new.sector',
                 'translation_domain' => 'CompanyBundle'
             ))
-            ->add('save',  'submit', array(
+            ->add('save', SubmitType::class, array(
                 'label' => 'company.new.submit',
                 'translation_domain' => 'CompanyBundle',
             ))
