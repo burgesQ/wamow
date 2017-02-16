7e20a814ef273191ebb27640f01c2d53b3c34cc5
jeromeheitor <jerome.heitor@epitech.eu>
2017-02-16 11:55:59 +0100

    [Internationalisation][PLAT-88] Added switching languages options

--- symfony/src/ToolsBundle/Controller/ToolsController.php
+++ symfony/src/ToolsBundle/Controller/ToolsController.php
@@ -7,7 +7,38 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Storage\PhpBridgeSessionStorage;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class ToolsController extends Controller
 {
+    /**
+     * Change the locale for the current user
+     *
+     * @param String $language
+     * @return array
+     *
+     * @Route("/setlocale/{language}", name="setlocale")
+     * @Template()
+     */
+    public function setLocaleAction($language = null)
+    {
+        if($language != null)
+        {
+            // On enregistre la langue en session
+            $this->get('session')->set('_locale', $language);
+        }
+
+        // on tente de rediriger vers la page d'origine
+        $url = $this->container->get('request')->headers->get('referer');
+        if(empty($url))
+        {
+            $url = $this->container->get('router')->generate('index');
+        }
+
+        return new RedirectResponse($url);
+    }
+
 }
