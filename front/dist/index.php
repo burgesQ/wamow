0a91804a407b70943ec89f6e2e7b17d6d8d11373
David Essayan <da.essayan@gmail.com>
2017-05-06 18:44:10 +0200

    Index file and pitch businesses minor fix

---
+++ front/dist/index.php
@@ -0,0 +1,49 @@
+<?php include "inc/header-onboarding.inc.php"; ?>
+
+		<div class="col-xs-10 col-xs-offset-1" style="text-align:center;">
+
+			<br /><br /><br />
+
+			<a href="advisor/onboarding-1.php" class="wmw-button">
+				<i></i><span>Advisor - On Boarding 1</span><i></i>
+			</a><br /><br />
+			<a href="advisor/onboarding-2.php" class="wmw-button">
+				<i></i><span>Advisor - On Boarding 2</span><i></i>
+			</a><br /><br />
+			<a href="advisor/onboarding-3.php" class="wmw-button">
+				<i></i><span>Advisor - On Boarding 3</span><i></i>
+			</a><br /><br />
+			<a href="advisor/onboarding-4.php" class="wmw-button">
+				<i></i><span>Advisor - On Boarding 4</span><i></i>
+			</a><br /><br />
+			<a href="advisor/dashboar.php" class="wmw-button">
+				<i></i><span>Advisor - Dashboard</span><i></i>
+			</a><br /><br />
+			<a href="contractor/pitch-1.php" class="wmw-button">
+				<i></i><span>Contractor - Pitch 1</span><i></i>
+			</a><br /><br />
+			<a href="contractor/pitch-2.php" class="wmw-button">
+				<i></i><span>Contractor - Pitch 2</span><i></i>
+			</a><br /><br />
+			<a href="contractor/pitch-3.php" class="wmw-button">
+				<i></i><span>Contractor - Pitch 3</span><i></i>
+			</a><br /><br />
+			<a href="contractor/pitch-4.php" class="wmw-button">
+				<i></i><span>Contractor - Pitch 4</span><i></i>
+			</a><br /><br />
+			<a href="contractor/dashboard-list.php" class="wmw-button">
+				<i></i><span>Contractor - Dashboard list</span><i></i>
+			</a><br /><br />
+			<a href="contractor/dashboard-shortlist.php" class="wmw-button">
+				<i></i><span>Contractor - Dashboard shortlist</span><i></i>
+			</a><br /><br />
+			<a href="contractor/dashboard-mail.php" class="wmw-button">
+				<i></i><span>Contractor - Dashboard chat</span><i></i>
+			</a><br /><br />
+			<a href="contractor/dashboard-winner.php" class="wmw-button">
+				<i></i><span>Contractor - Dashboard winner</span><i></i>
+			</a><br /><br />
+
+		</div>
+
+<?php include "inc/footer.inc.php"; ?>
