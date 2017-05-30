ac86a6acf5bc72aedbb15dd9029228e525a6bfef
Quentin Burgess <qutn.burgess@gmail.com>
2017-05-30 12:54:07 +0200

    [201-missiong-trans][#201]
    - [x] patch mising var for trans
    - [x] patch redirection login error
    - [x] patch send password
    
    close #201

--- symfony/src/UserBundle/Controller/SecurityController.php
+++ symfony/src/UserBundle/Controller/SecurityController.php
@@ -7,16 +7,17 @@ use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends BaseController
 {
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
     public function loginAction(Request $request)
     {
         if ($this->getUser() !== null) {
+
             return $this->redirectToRoute('dashboard');
         }
 
-        return $this->render('@User/Security/login.html.twig', [
-            'last_username' => null,
-            'error'         => null,
-            'csrf_token'    => $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
-        ]);
+        return parent::loginAction($request);
     }
 }
