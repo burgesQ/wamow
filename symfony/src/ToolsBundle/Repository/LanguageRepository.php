b4847a8183c4514e0b87b32c61cae5397a22b1d3
Quentin Burgess <qutn.burgess@gmail.com>
2017-06-14 17:51:04 +0200

    [101-integration-edit-profile][#101]
    - [x] need to complete Adv
    - [x] need to complete Ctr
    - [x] need review

--- symfony/src/ToolsBundle/Repository/LanguageRepository.php
+++ symfony/src/ToolsBundle/Repository/LanguageRepository.php
@@ -2,7 +2,10 @@
 
 namespace ToolsBundle\Repository;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\ORM\EntityRepository;
+use InvalidArgumentException;
 
 /**
  * LanguageRepository
@@ -12,4 +15,35 @@ use Doctrine\ORM\EntityRepository;
  */
 class LanguageRepository extends EntityRepository
 {
+    /**
+     * @param $q
+     * @param $page
+     * @param $pageLimit
+     * @return \ToolsBundle\Repository\Paginator
+     */
+    public function findAutocompleteCertification($q, $page, $pageLimit)
+    {
+
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('l')
+            ->from('ToolsBundle:Language', 'l')
+            ->where('l.name LIKE :q')
+            ->setParameter('q', $q . '%')
+        ;
+
+        if (!is_numeric($page)) {
+            throw new InvalidArgumentException(
+                'La valeur de l\'argument $page est incorrecte (valeur : ' . $page . ' ).');
+        } elseif (!is_numeric($pageLimit)) {
+            throw new InvalidArgumentException(
+                'La valeur de l\'argument $pageLimit est incorrecte (valeur : ' . $pageLimit . ' ).');
+        } elseif ($page < 1 || $pageLimit < 1) {
+            throw new NotFoundHttpException('La page demandée n\'existe pas');
+        }
+        $firstResult = ($page - 1) * $pageLimit;
+        $query       = $qb->getQuery();
+        $query->setFirstResult($firstResult)->setMaxResults($pageLimit);
+
+        return new Paginator($query);
+    }
 }
