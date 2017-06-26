929e4c126e92f9ab01a47b9a655424c183120a90
David Essayan <da.essayan@gmail.com>
2017-06-26 11:59:07 +0200

    Gruntfile moved to symfony/web/

---
+++ symfony/web/src/js/auditors.js
@@ -0,0 +1,9 @@
+var Auditors = [
+	
+	{ value: "Bernard Dupuis" },
+	{ value: "Georges Dupont" },
+	{ value: "Michael Flanagan" },
+	{ value: "David Essayan" },
+	{ value: "Victorien Leleu" },
+	{ value: "Gandhi" },
+];
