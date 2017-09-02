39cc870440eb92b1d8ce2cfe8e6a93f369912ed5
Quentin Burgess <qutn.burgess@gmail.com>
2017-09-02 13:41:52 +0200

    oder list for select; checkbox otw.

--- symfony/web/js/scripts.js
+++ symfony/web/js/scripts.js
@@ -2994,41 +2994,66 @@ var Auditors = [
 // --------------------- MASTER PART -------------------------- //
 // ------------------------------------------------------------ //
 
-(function($) {
+function sortSelectOptions(selector, skip_first) {
 
-$.fn.deserialize = function (serializedString)
-{
-    var $form = $(this);
-    $form[0].reset();
-    serializedString = serializedString.replace(/\+/g, '%20');
-    var formFieldArray = serializedString.split("&");
+    var options = (skip_first) ? $(selector + ' option:not(:first)') : $(selector + ' option');
 
-    $.each(formFieldArray, function(i, pair){
-        var nameValue = pair.split("=");
-        if(nameValue[0] !== null){
-            var name = decodeURIComponent(nameValue[0]);
-            var value = decodeURIComponent(nameValue[1]);
-            // Find one or more fields
-            var $field = $form.find('[name="' + name + '"]');
+    var arr = options.map(function(_, o) {
+        return { t: $(o).text(), v: o.value, s: $(o).prop('selected') };
+    }).get();
 
-            console.log($field, name);
+    arr.sort(function(o1, o2) {
+        var t1 = o1.t.toLowerCase(), t2 = o2.t.toLowerCase();
+        return t1 > t2 ? 1 : t1 < t2 ? -1 : 0;
+    });
 
-            if ($field[0].type == "radio"
-                || $field[0].type == "checkbox")
-            {
-                var $fieldWithValue = $field.filter('[value="' + value + '"]');
-                var isFound = ($fieldWithValue.length > 0);
-                if (!isFound && value == "on") {
-                    $field.first().prop("checked", true);
-                } else {
-                    $fieldWithValue.prop("checked", isFound);
-                }
-            } else {
-                $field.val(value);
-            }
+    options.each(function(i, o) {
+        o.value = arr[i].v;
+        $(o).text(arr[i].t);
+        if (arr[i].s) {
+            $(o).attr('selected', 'selected').prop('selected', true);
+        } else {
+            $(o).removeAttr('selected');
+            $(o).prop('selected', false);
         }
     });
 }
+
+(function($) {
+
+    $.fn.deserialize = function (serializedString)
+    {
+        var $form = $(this);
+        $form[0].reset();
+        serializedString = serializedString.replace(/\+/g, '%20');
+        var formFieldArray = serializedString.split("&");
+
+        $.each(formFieldArray, function(i, pair){
+            var nameValue = pair.split("=");
+            if(nameValue[0] !== null){
+                var name = decodeURIComponent(nameValue[0]);
+                var value = decodeURIComponent(nameValue[1]);
+                // Find one or more fields
+                var $field = $form.find('[name="' + name + '"]');
+
+                console.log($field, name);
+
+                if ($field[0].type == "radio"
+                    || $field[0].type == "checkbox")
+                {
+                    var $fieldWithValue = $field.filter('[value="' + value + '"]');
+                    var isFound = ($fieldWithValue.length > 0);
+                    if (!isFound && value == "on") {
+                        $field.first().prop("checked", true);
+                    } else {
+                        $fieldWithValue.prop("checked", isFound);
+                    }
+                } else {
+                    $field.val(value);
+                }
+            }
+        });
+    }
 
 var Master = {
 
