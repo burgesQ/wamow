d43bf62ec1457480f92ab3bdbf402d926a153cde
Quentin Burgess <qutn.burgess@gmail.com>
2017-10-03 14:24:42 +0200

    update work exp step 3 mission gen

--- symfony/src/MissionBundle/Controller/ActionController.php
+++ symfony/src/MissionBundle/Controller/ActionController.php
@@ -105,9 +105,71 @@ class ActionController extends Controller
             // NOTE : Flush or not ?
             $this->getDoctrine()->getManager()->flush();
 
-            return new JsonResponse(array("matches" => count($this->getDoctrine()->getManager()->getRepository("MissionBundle:Mission")->findUsersByMission($mission))));
+            return new JsonResponse([
+                "matches" => count($this->getDoctrine()->getManager()->getRepository("MissionBundle:Mission")
+                    ->findUsersByMission($mission))
+            ]);
         }
 
         throw $this->createNotFoundException($trans->trans('mission.error.forbiddenAccess', [], 'MissionBundle'));
     }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param                                           $missionId
+     * @return \Symfony\Component\HttpFoundation\JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function getWorkExpAction(Request $request, $missionId)
+    {
+        $em           = $this->getDoctrine()->getManager();
+        $mission      = $em->getRepository('MissionBundle:Mission')->findOneBy(['id' => $missionId]);
+        $user         = $this->getUser();
+        $trans = $this->get('translator');
+
+        // NOTE : Duplicate stepThreeAction
+
+        $missionRepository = $this->getDoctrine()->getRepository('MissionBundle:Mission');
+        /** @var \UserBundle\Entity\User $user */
+        $user = $this->getUser();
+        /** @var \MissionBundle\Entity\Mission $newMission */
+        if (!($newMission = $missionRepository->findOneBy(['id' => $missionId, 'contact' => $user]))) {
+            return new Response($this->get('translator')->trans('mission.error.authorized', [], 'MissionBundle'));
+        }
+
+        $stepMission = $newMission->getStatusGenerator();
+        if ($stepMission !== Mission::STEP_TWO && $stepMission !== Mission::STEP_THREE) {
+            return $this->redirectToRoute('mission_edit', ['missionId' => $newMission->getId()]);
+        }
+
+        $newMission->setOnDraft(true);
+        $arrayForm = [];
+
+        if ($stepMission === Mission::STEP_THREE) {
+            $arrayForm['stepFour']  = 'display: none;';
+            $arrayForm['labelBack'] = 'mission.new.form.back_resume';
+            $arrayForm['labelNext'] = 'mission.new.form.save_mod';
+        }
+
+        /** @var \Symfony\Component\Form\Form $formStepThree */
+        $formStepThree = $this->createForm(new StepThreeFormType(), $newMission, $arrayForm)->setData($newMission);
+
+        if ($formStepThree->handleRequest($request)->isSubmitted() && $formStepThree->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            $workExp = $this->getDoctrine()->getRepository("MissionBundle:WorkExperience")
+                ->findWorkExp($mission);
+            $array = [];
+            foreach ($workExp as $oneWork) {
+                $array += [$oneWork->getId() => $trans->trans($oneWork->getName(), [], "tools")];
+            }
+            return new JsonResponse([
+                "work_experiences" => $array
+            ]);
+        }
+
+        throw $this->createNotFoundException($trans->trans('mission.error.forbiddenAccess', [], 'MissionBundle'));
+    }
+
+
+
 }
