78a1990d0df2b6507ad65ec400a52f91e605b017
Quentin Burgess <qutn.burgess@gmail.com>
2017-10-10 21:11:47 +0200

    patch deploy file

--- symfony/app/config/deploy/staging.rb
+++ symfony/app/config/deploy/staging.rb
@@ -12,7 +12,6 @@ set :application, "WantMoreWork-Plateform"
 set :app_path,    "app"
 # Server conf
 set :domain,      "app@staging.wamow.co"
-# set :domain,      "app@staging.consultants.wantmore.work"
 set :use_sudo,    false
 
 role :web,        domain                         # Your HTTP server, Apache/etc
@@ -22,7 +21,6 @@ role :db,         domain, :primary => true       # This is where Rails migration
 set :deploy_to,     "/home/app/www/staging/"
 set :current_stage, "staging"
 set :url_base,      "staging.wamow.co"
-# set :url_base,      "staging.consultants.wantmore.work"
 
 #Git stagging branch
 set :branch, "preprod"
