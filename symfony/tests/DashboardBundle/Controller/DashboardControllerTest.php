4f99202b67b96e6b10e8e99761062ada0f676c46
burgesQ <qutn.burgess@gmail.com>
2018-02-21 14:41:01 +0100

    add: test for dashboard  bundle

---
+++ symfony/tests/DashboardBundle/Controller/DashboardControllerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace DashboardBundle\Tests\Controller;
+
+use ToolsBundle\Tests\WamowTestCase;
+
+class DashboardControllerTest extends WamowTestCase
+{
+    /**
+     * Test get dashboard not logged
+     * Test get advisor dashboard
+     * Test get contractor dashboard
+     */
+    public function testDashboardAction()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/en/dashboard');
+        self::assertTrue(
+            $client->getResponse()->isClientError()
+        );
+
+        $crawler = $this->doLogin("contractor@one.com", "password");
+        // follow / to /dash
+        $crawler = $this->client->followRedirect();
+        // follow /dash to /en/dash
+        $crawler = $this->client->followRedirect();
+
+        self::assertEquals(
+            1,
+            $crawler->filter("a.sidebar-progress-button")->count()
+        );
+
+        $crawler = $this->doLogin("fake-vanessa.albek@gt-executive.com", "password");
+        // follow / to /dash
+        $crawler = $this->client->followRedirect();
+        // follow /dash to /en/dash
+        $crawler = $this->client->followRedirect();
+
+        self::assertEquals(
+            0,
+            $crawler->filter("a.sidebar-progress-button")->count()
+        );
+    }
+}
